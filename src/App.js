2fe4c9a8023b5ac6567415b30a7a39345a67556a
himanshuc11 <himanshuchhatpar@gmail.com>
2022-01-04 16:25:55 +0530

    Working Prototype

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
 import './App.css';
 import Draggable from 'react-draggable';
-import { ResizableBox } from 'react-resizable';
 import { useState } from 'react';
+
+import Toast from './Toast';
 
 function App() {
 
-  let [sizes, setSizes] = useState({width: "100px", height: "100px"})
-
-  let onResize = (event, {element, size, handle}) => {
-    // this.setState({width: size.width, height: size.height});
-    setSizes({width: sizes.width, height: size.height})
-  };
+  let [isDraggable, setIsDraggable] = useState(true)
+  let [show, setShow] = useState(false)
 
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <Draggable>
-          <p>This is draggable</p>
+        <Draggable disabled={isDraggable} onStop={() => {setShow(true)
+          setTimeout(() => setShow(false), 2000)
+        }}> 
+          <p data-tip="This is tooltip" className='tooltip'>This is draggable</p>
         </Draggable>
-        
-        <ResizableBox width={200} height={200} onResize={onResize}>
-        <span>Contents</span>
-      </ResizableBox>
-      </header>
 
+        {show ? <Toast></Toast>: <></>}
+
+        <button onClick={() => setIsDraggable(!isDraggable)}>{isDraggable ? "Enable Drag": "Disable Drag"}</button>
+        
 
     </div>
   );
---
+++ src/Toast.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+const Toast = () => {
+
+    return (
+        <div>
+           Drag happened Successfully
+        </div>
+    )
+}
+
+export default Toast
